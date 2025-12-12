ef5381e3c63a1dbf266ae4b41174f811669f27ee
Oscar-henri STEFANINI <oscar.stefanini1@gmail.com>
2025-12-12 17:15:14 +0100

    wip

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,43 +1,102 @@
 import { useEffect, useState } from "react";
-import { type Asset } from "@ts-drones/shared";
+import { type Asset, type Tag, assetCreateSchema } from "@ts-drones/shared";
 import '@mantine/core/styles.css';
-import { Center, NativeSelect, Title, MantineProvider, Input, Button } from '@mantine/core';
+import { Center, Title, Input, Button, Card, Image, Group, Text, Badge, SimpleGrid } from '@mantine/core';
+import { serialize } from 'object-to-formdata';
+import { IconDatabase, IconSearch } from "@tabler/icons-react";
 
-import axios from "axios";
-import { IconSearch } from "@tabler/icons-react";
+import api from "./services/api";
+import { demoPlaystationModels } from "./constants";
+
 
 function App() {
   const [assets, setAssets] = useState<Asset[]>([]);
+  const [tags, setTags] = useState<Tag[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get<Asset[]>("http://localhost:4000/assets")
-      .then(response => setAssets(response.data))
-      .finally(() => { setLoading(false) })
-      .catch((error: Error) => {
-        console.error("Error fetching assets:", error);
-        setLoading(false);
-      });
+    let cancelled = false;
+
+    async function load() {
+      try {
+        const [{ data: a }, { data: b }] = await Promise.all([
+          api.get<Asset[]>("/assets"),
+          api.get<Tag[]>("/tags"),
+        ]);
+
+        if (!cancelled) {
+          setAssets(a);
+          setTags(b);
+          setLoading(false);
+        }
+      } catch (e) {
+        if (!cancelled) {
+          console.error(e);
+        }
+      }
+    }
+
+    load();
+    return () => {
+      cancelled = true;
+    };
   }, []);
 
   if (loading) return <div>Chargement…</div>;
 
   return (
-    <MantineProvider
-      defaultColorScheme="dark"
-      forceColorScheme="dark"
+    <div
+      style={{ marginTop: "75px" }}
     >
-      <div
-        style={{ marginTop: "75px" }}
-      >
-        <Center><Title order={1}>TS - Drones</Title></Center>
-        <Center><Title order={3}>A minimalist version of the drawn lights app, in typescript</Title></Center>
-        <div style={{ marginTop: "24px" }}>
-          {assets.length === 0 && (
-            <Center>No asset yet, create the first one !</Center>
-            
-          )}
-          {assets.length > 0 && (
+      <Center><Title order={1}>TS - Drones</Title></Center>
+      <Center><Title order={3}>A minimalist version of the drawn lights app, in typescript</Title></Center>
+      <div style={{ marginTop: "24px" }}>
+        {assets.length === 0 && (
+          <div>
+            <Center>No asset yet, let's populate the database !</Center>
+            <Center style={{ marginTop: "12px" }}>
+              <Button
+                onClick={async () => {
+                  try {
+                    await Promise.all(demoPlaystationModels.map(async (model) => {
+                      const filename = `${model.name}.png`;
+                      const res = await fetch(`/public/${filename}`);
+                      const imgBlob = await res.blob();
+                      const sendData = {
+                        thumbnail: new File([imgBlob], filename, { type: "image/png" }),
+                        description: null,
+                        name: model.name,
+                        durationSec: model.duration,
+                        nbUav: model.nbUav,
+                        priceEur: 0,
+                        type: "2d",
+                        tags: ["playstation"],
+                      }
+                      const safeData = assetCreateSchema.safeParse(sendData);
+                      if (!safeData.success) {
+                        console.error(safeData.error);
+                        throw new Error("Invalid asset data");
+                      }
+
+                      const serializeData = serialize(safeData.data);
+                      console.log(serializeData);
+
+                      const { data } = await api.post<Asset>("/assets", serializeData)
+                      setAssets((prev) => [...prev, data]);
+                    }))
+                  } catch (e) {
+                    console.error(e);
+                  }
+                }}
+              >
+                Populate
+                <IconDatabase />
+              </Button>
+            </Center>
+          </div>
+        )}
+        {assets.length > 0 && (
+          <>
             <Center>
               <Input
                 placeholder='search for an asset...'
@@ -47,10 +106,39 @@ function App() {
                 style={{ marginLeft: "12px" }}
               ><IconSearch /></Button>
             </Center>
-          )}
-        </div>
+            <SimpleGrid
+              cols={{ xs: 1, sm: 2, md: 3, lg: 4 }}
+              spacing="md"
+              style={{ margin: "24px" }}
+            >
+              {assets.map((asset) => (
+                <Card>
+                  <Card.Section>
+                    <Image
+                      height={200}
+                      width={"auto"}
+                      src={`http://localhost:4000/assets/thumbnail/${asset.thumbnail}`} alt={asset.name} />
+                  </Card.Section>
+                  <Group justify="space-between" mt="md" mb="xs">
+                    <Text fw={500}>{asset.name}</Text>
+                    {asset.tags.map((tag) => <Badge color="pink" key={tag}>{tag}</Badge>)}
+                  </Group>
+
+                  {asset.description && <Text size="sm" c="dimmed">
+                    {asset.description}
+                  </Text>}
+
+                  <Button color="blue" fullWidth mt="md" radius="md">
+                    Book classic tour now
+                  </Button>
+                </Card>
+              ))}
+            </SimpleGrid>
+          </>
+        )
+        }
       </div>
-    </MantineProvider>
+    </div >
   );
 }
 
---
+++ apps/web/src/constants.ts
@@ -0,0 +1,32 @@
+const demoPlaystationModels = [
+    {
+        "_id": "65f21d24cfe550e24cf095a9",
+        "name": "cross",
+        "videoLink": "https://drive.google.com/file/d/1-mBMrv0zWrOqu3DhMh9OqjY-mpFBqyOY/preview",
+        "duration": 2,
+        "nbUav": 12,
+    },
+    {
+        "_id": "65f21da2cfe550e24cf095aa",
+        "videoLink": "https://drive.google.com/file/d/1t9gpRGxepRR52eGweApXXaI2yC0lJ4rG/preview",
+        "name": "circle",
+        "duration": 2,
+        "nbUav": 12,
+    },
+    {
+        "_id": "65f21df1cfe550e24cf095ac",
+        "name": "square",
+        "videoLink": "https://drive.google.com/file/d/1vpClM23ibPXewKxsRNgrjv1r4axsp_fG/preview",
+        "duration": 2,
+        "nbUav": 12,
+    },
+    {
+        "_id": "65f21dc8cfe550e24cf095ab",
+        "name": "triangle",
+        "videoLink": "https://drive.google.com/file/d/1jmr0Iq2qsIvS9nqu-a6A6wK17jI1uIJ1/preview",
+        "duration": 2,
+        "nbUav": 12,
+    }
+]
+
+export { demoPlaystationModels };

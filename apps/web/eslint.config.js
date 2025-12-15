124908e48229b7ee47855d6d7ddbe1049cd3ca7e
Oscar-henri STEFANINI <oscar.stefanini1@gmail.com>
2025-12-15 21:53:41 +0100

    infra(*): try ci/cd

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -3,11 +3,12 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import reactX from 'eslint-plugin-react-x'
 import reactDom from 'eslint-plugin-react-dom'
 
-export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -29,4 +30,4 @@ export default tseslint.config(
       },
     },
   },
-)
+])
